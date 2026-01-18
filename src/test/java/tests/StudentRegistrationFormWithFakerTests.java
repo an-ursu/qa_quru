665eb4e62b399f3fb97eb426558efca7d12b0aaa
Anastasiya Ursu <a-ursu20@yandex.ru>
2026-01-18 16:54:56 +0300

    added changes at StudentRegistrationFormWithFakerTests

--- src/test/java/tests/StudentRegistrationFormWithFakerTests.java
+++ src/test/java/tests/StudentRegistrationFormWithFakerTests.java
@@ -1,8 +1,12 @@
 package tests;
 
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 import utils.RandomUtils;
+
+import static io.qameta.allure.Allure.step;
 
 
 public class StudentRegistrationFormWithFakerTests extends TestBase {
@@ -10,10 +14,51 @@ public class StudentRegistrationFormWithFakerTests extends TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
     RandomUtils randomUtils = new RandomUtils();
 
+    @Tag("demoqa")
     @Test
     void registrationFormFullRequiredFieldsTest() {
 
-        registrationPage.openPage()
+        step("Open page", () -> {
+            registrationPage.openPage()
+                    .removeBanners();
+        });
+
+        step("Fill form on page", () -> {
+            registrationPage.setFirstName(randomUtils.firstName)
+                    .setLastName(randomUtils.lastName)
+                    .setEmail(randomUtils.email)
+                    .setGenter(randomUtils.gender)
+                    .setUserPhoneNumber(randomUtils.phoneNumber)
+                    .setDateOfBirthday(randomUtils.birthdayDay, randomUtils.birthdayMonth, randomUtils.birthdayYear)
+                    .setSubjects(randomUtils.subjects)
+                    .setHobbies(randomUtils.hobbies)
+                    .setPicture(randomUtils.picture)
+                    .setAddress(randomUtils.address)
+                    .setState(randomUtils.state)
+                    .setCity(randomUtils.city);
+
+        });
+
+        step("Submit", () -> {
+            registrationPage.setSubmit();
+        });
+
+        step("Check result", () -> {
+            registrationPage.checkResult(randomUtils.firstName + " " + randomUtils.lastName)
+                    .checkResult(randomUtils.email)
+                    .checkResult(randomUtils.gender)
+                    .checkResult(randomUtils.phoneNumber)
+                    .checkResult(randomUtils.birthdayDay + " " + randomUtils.birthdayMonth + "," + randomUtils.birthdayYear)
+                    .checkResult(randomUtils.subjects)
+                    .checkResult(randomUtils.hobbies)
+                    .checkResult(randomUtils.picture)
+                    .checkResult(randomUtils.address)
+                    .checkResult(randomUtils.state + " " + randomUtils.city);
+        });
+    }
+}
+
+       /* registrationPage.openPage()
                 .removeBanners()
                 .setFirstName(randomUtils.firstName)
                 .setLastName(randomUtils.lastName)
@@ -40,9 +85,9 @@ public class StudentRegistrationFormWithFakerTests extends TestBase {
                 .checkResult(randomUtils.address)
                 .checkResult(randomUtils.state + " " + randomUtils.city);
 
-    }
+    }*/
 
-    @Test
+    /*@Test
     void registrationFormMinimalRequiredFieldsTest() {
 
         registrationPage.openPage()
@@ -80,3 +125,4 @@ public class StudentRegistrationFormWithFakerTests extends TestBase {
 
 }
 
+*/
