b6df58a1c081a13c72a35302a69ca23fe7f1170f
Anastasiya Ursu <a-ursu20@yandex.ru>
2026-01-18 17:24:50 +0300

    added changes at Test Base

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,14 +4,12 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
@@ -21,8 +19,6 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
@@ -32,11 +28,10 @@ public class TestBase {
 
     }
 
-
-/*    @AfterEach
-    void tearDown() {
-        closeWebDriver();
-    }*/
+    @BeforeEach
+    void beforeEach() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
 
     @AfterEach
     void addAttachments() {
