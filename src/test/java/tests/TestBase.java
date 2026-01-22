a723add83f79496fa3309e9486eacbff00ff28d7
Anastasiya Ursu <a-ursu20@yandex.ru>
2026-01-22 12:41:41 +0300

    add changes for HW-11

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -20,13 +20,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = System.getProperty("remote");
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 15000;
         Configuration.browser = System.getProperty("BROWSER", "chrome");
-        if (Configuration.browser.equals("firefox")) {
-            Configuration.fastSetValue = false; // Важно для Firefox!
-            Configuration.clickViaJs = true;    // Использовать JS для кликов
-            Configuration.timeout = 20000;      // Увеличить таймаут
-        }
         Configuration.browserVersion = System.getProperty("BROWSER_VERSION", "128.0");
         Configuration.browserSize = System.getProperty("BROWSER_SIZE", "1920x1080");
 
